a34655fb43b7b3836b6181bce82af0d8e7103d83
rumi-000 <rumi.sato9797@gmail.com>
2023-03-05 09:11:16 +0000

    Initial Commit

--- bookers2/app/javascript/packs/application.js
+++ bookers2/app/javascript/packs/application.js
@@ -7,8 +7,7 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-//= require rails-ujs
-// import "jquery"
+import "jquery"
 import "popper.js"
 import "bootstrap"
 import '@fortawesome/fontawesome-free/js/all';
@@ -19,6 +18,7 @@ import "../stylesheets/application"
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
 
 
 
