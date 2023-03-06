9ab096698eceedb426ecab440afc9583ba1a69ac
rumi-000 <rumi.sato9797@gmail.com>
2023-03-06 23:54:30 +0000

    Initial Commit

--- bookers2/app/models/user.rb
+++ bookers2/app/models/user.rb
@@ -9,26 +9,35 @@ class User < ApplicationRecord
   has_many :book_comments, dependent: :destroy
   
   
+  # フォローされている状況
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  # フォロワー
+  has_many :followers, through: :reverse_of_relationships, source: :follower
+  
+  # フォローする状況
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  # フォローした人
+  has_many :followings, through: :relationships, source: :followed
+  
   has_one_attached :profile_image
   
-  
-
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, length: { maximum: 50 }
   
-
-  def get_profile_image (width,height)
+ def get_profile_image (width,height)
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
-  end
+ end
  
+  def follow(user)
+    relationships.create(followed_id: user.id)
+  end
 
+  def unfollow(user)
+    relationships.find_by(followed_id: user.id).destroy
+  end
+
+  def following?(user)
+    followings.include?(user)
+  end
   
-  #def get_profile_image
-  #unless profile_image.attached?
-   #file_path = Rails.root.join('app/assets/images/no-image-icon.jpg')
-    #profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
-  #end
-  #profile_image.variant(resize_to_limit: [width, height]).processed
-#end
-
 end
